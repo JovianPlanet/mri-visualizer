c6fea5ee50c48b21fb9385a3270ee2aeccf1e8d6
JovianPlanet <davidpunk@gmail.com>
2023-05-11 18:28:54 -0500

    Cleaned code

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 import nibabel as nib
 import numpy as np
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore
 from popups import ErrorDialog
 import GUI
 from utils import conv2QImage
@@ -15,9 +15,8 @@ class GuiSetUp(QtWidgets.QMainWindow, GUI.Ui_Form):
         self.setupUi(self)
         self.browse_button.clicked.connect(self.get_path)
         self.loadSlice_button.clicked.connect(self.load_slice)
+        self.sliceSlider.sliderMoved.connect(self.slide)
         self.loadMRI_button.clicked.connect(self.load_mri)
-
-        #self.sliceSlider.valueChanged.connect(self.make_)
 
         self.axial_cb.stateChanged.connect(self.update_gui)
         self.sagital_cb.stateChanged.connect(self.update_gui)
@@ -31,12 +30,15 @@ class GuiSetUp(QtWidgets.QMainWindow, GUI.Ui_Form):
 
         self.vol = None
 
+    # Función que captura la ruta donde se encuentra la imagen
     def get_path(self):
 
         self.path, _ = QtWidgets.QFileDialog.getOpenFileName(self, "Open File", "Images (*.nii *.nii.gz)")
         self.path_edit.setText(self.path)
         return
 
+    # Función que carga la imagen, ubica la vista que se quiere desplegar y crea la escena
+    # a partir del slice central
     def load_mri(self):
         self.vol = nib.load(self.path).get_data()
         print(f'{self.vol.shape=}, type - {self.vol.dtype}')
@@ -48,61 +50,81 @@ class GuiSetUp(QtWidgets.QMainWindow, GUI.Ui_Form):
             self.sliceSlider.setMaximum(self.vol.shape[2])
             self.sliceSlider.setValue(self.vol.shape[2]/2)
             self.pixmap = conv2QImage(self.vol[:, :, self.sliceSlider.value()])
+            self.scene = QtWidgets.QGraphicsScene(0, 0, self.vol.shape[0], self.vol.shape[1])
         elif self.sagital_cb.isChecked():
             self.view = 'sagital'
             self.sliceSlider.setMaximum(self.vol.shape[1])
             self.sliceSlider.setValue(self.vol.shape[1]/2)
             self.pixmap = conv2QImage(self.vol[:, self.sliceSlider.value(), :])
+            self.scene = QtWidgets.QGraphicsScene(0, 0, self.vol.shape[0], self.vol.shape[2])
         elif self.coronal_cb.isChecked():
             self.view = 'coronal'
             self.sliceSlider.setMaximum(self.vol.shape[0])
             self.sliceSlider.setValue(self.vol.shape[0]/2)
             self.pixmap = conv2QImage(self.vol[self.sliceSlider.value(), :, :])
+            self.scene = QtWidgets.QGraphicsScene(0, 0, self.vol.shape[1], self.vol.shape[2])
         else:
             self.show_message('ERROR: Debe seleccionar una vista antes de cargar la imagen', False)
             return
 
-        self.scene = QtWidgets.QGraphicsScene(0, 0, self.vol.shape[0], self.vol.shape[1])
-
         pixmapitem = self.scene.addPixmap(self.pixmap)
         pixmapitem.setPos(0, 0)
 
+        #self.graphicsView.fitInView(pixmapitem, QtCore.Qt.KeepAspectRatio)
+
         self.graphicsView.setScene(self.scene)
         self.graphicsView.show()
-        #app.exec_()
         return
 
+    # Función para cargar el slice, especificado en el campo de texto, al presionar el botón 'Load'
     def load_slice(self):
         try:
-            slicenum = int(self.lineEdit_slice.text())
+            n = int(self.lineEdit_slice.text())
         except:
-            self.show_message('ERROR: Inserte un valor númerico valido', False)
+            self.show_message('ERROR: Ingrese un valor númerico valido', False)
+            return    
+
+        self.update_scene(n)       
+
+        return
+
+    # Función que permite "deslizarse" a lo largo de todos los slices de la imagen
+    def slide(self):
+        try:
+            n = int(self.sliceSlider.value())
+        except:
+            self.show_message('ERROR: Ingrese un valor númerico valido', False)
             return
 
+        self.update_scene(n)
+        return
+
+    # Función que actualiza la escena de acuerdo con el slice especificado
+    def update_scene(self, s):
+
         if self.view == 'axial':
-            if slicenum > self.vol.shape[2]:
+            if s > self.vol.shape[2]:
                 self.show_message('ERROR: El número de slice seleccionado es mayor al número total de slices', False)
                 return
-            self.pixmap = conv2QImage(self.vol[:, :, slicenum])
+            self.pixmap = conv2QImage(self.vol[:, :, s])
         elif self.view == 'sagital':
-            if slicenum > self.vol.shape[1]:
+            if s > self.vol.shape[1]:
                 self.show_message('ERROR: El número de slice seleccionado es mayor al número total de slices', False)
                 return
-            self.pixmap = conv2QImage(self.vol[:, slicenum, :])
+            self.pixmap = conv2QImage(self.vol[:, s, :])
         elif self.view == 'coronal':
-            if slicenum > self.vol.shape[0]:
+            if s > self.vol.shape[0]:
                 self.show_message('ERROR: El número de slice seleccionado es mayor al número total de slices', False)
                 return
-            self.pixmap = conv2QImage(self.vol[slicenum, :, :])
+            self.pixmap = conv2QImage(self.vol[s, :, :])
         else:
             self.show_message('ERROR: Aún no se ha seleccionado una vista', False)
         pixmapitem = self.scene.addPixmap(self.pixmap)
         pixmapitem.setPos(0, 0)
         self.scene.update()
-
         return
 
-
+    # Función que actualiza la GUI cuando se activa o desactiva un campo de selección
     def update_gui(self):
 
         if self.axial_cb.isChecked():
@@ -123,6 +145,7 @@ class GuiSetUp(QtWidgets.QMainWindow, GUI.Ui_Form):
             self.coronal_cb.setEnabled(True)
 
 
+    # Función para desplegar los mensajes de error
     def show_message(self, s, rem):
         print("click", s)
 
@@ -142,69 +165,3 @@ view = QGraphicsView(scene)
 view.show()
 app.exec_()
 '''
-
-
-# def make_dl(self):
-
-#     flag=False
-
-#     try:
-#         with open('dl.py', 'x') as dl:
-#             for line in template.splitlines(keepends=True):
-
-#                 if "self.path = './data/train'" in line:
-#                     dl.write(line.replace('./data/train', self.path))
-
-#                 elif "self.mri_name = 'T1.nii'" in line:
-#                     if self.load_mri_cb.isChecked():
-#                         dl.write(line.replace('T1.nii', self.mri_name_le.text()))
-#                     else:
-#                         dl.write(line)
-
-#                 elif "self.labels_name = None" in line:
-#                     if self.load_labels_cb.isChecked():
-#                         dl.write(line.replace('None', "'"+self.label_name_le.text()+"'"))
-#                     else:
-#                         dl.write(line)
-
-#                 elif "self.dims = False" in line:
-#                     if self.resize_cb.isChecked():
-#                         dl.write(line.replace('False', f'({self.width_spinbox.value()}, {self.height_spinbox.value()})', -1))
-#                     else:
-#                         dl.write(line)
-
-#                 elif "self.remove_bgnd = False" in line:
-#                     if self.bgnd_cb.isChecked():
-#                         dl.write(line.replace('False', 'True'))
-#                     else:
-#                         dl.write(line)
-
-#                 elif "self.crop = False" in line:
-#                     print(f'crop false in line')
-#                     if self.crop_cb.isChecked():
-#                         print(f'crop is checked')
-#                         if self.low_slice_sb.value() < self.up_slice_sb.value():
-#                             print('Es menor')
-#                             dl.write(line.replace('False', f'({self.low_slice_sb.value()}, {self.up_slice_sb.value()})', -1))
-#                         else:
-#                             print('Es mayor')
-#                             self.show_message('ERROR: Low slice must be smaller than upper slice', True)
-#                             flag=True
-#                             break
-#                     else:
-#                         dl.write(line)
-
-#                 else:
-#                     dl.write(line)
-
-#     except:
-#         self.show_message('El archivo ya existe, por favor borrelo e intente de nuevo', False)
-#         return
-
-#     if flag:
-#         os.remove('dl.py')
-#     else:
-#         self.show_message('El dataset fue creado exitosamente', False)
-#         self.close()
-
-#     return
--- utils.py
+++ utils.py
@@ -11,11 +11,8 @@ def conv2QImage(im):
     # bytesPerLine = width
 
     img = Image.fromarray(im[:,:]/im.max()*255).convert('RGB')#(im[:,:,80], mode="RGBA")#
-    print(f'PIL tipo - {type(img)}')
+    #print(f'PIL tipo - {type(img)}')
     qim = ImageQt.ImageQt(img)
     pm = QtGui.QPixmap.fromImage(qim)
-
-    #qimg = QtGui.QImage(im[:,:,100], width, height, bytesPerLine, QtGui.QImage.Format_RGB888)#
-    #pixmap = QtGui.QPixmap.fromImage(im[:,:,100])
 
     return pm
